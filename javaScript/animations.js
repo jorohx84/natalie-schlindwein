7e204bdc7ffe7c12f2caf13eab4f177c164f7fde
Johannes Roth <jrothx@icloud.com>
2025-04-11 14:58:16 +0200

    hero section works

--- javaScript/animations.js
+++ javaScript/animations.js
@@ -1,11 +1,24 @@
 let hide = false;
+
+
+
+function resetRespMenu() {
+    if (window.innerWidth > 780) {
+        if (hide===true) {
+         toogleRespMenu();
+
+        }
+       
+        
+    }
+
+}
+
+
 function burgerAnimation() {
     const burgerRef = document.getElementById('burger');
-    
-    console.log('burger');
-
     burgerRef.classList.toggle('open');
-   
+
 
 }
 
@@ -14,11 +27,14 @@ function toogleRespMenu() {
     burgerAnimation();
     const respMenuRef = document.getElementById('respMenu');
     const contentRef = document.getElementById('content');
+    const headerRef = document.getElementById('headerContainer');
+
     console.log(hide);
     if (respMenuRef) {
         if (hide) {
             respMenuRef.classList.add('slideIn');
             contentRef.classList.add('hideContent');
+            headerRef.classList.add('blackBG');
             setTimeout(() => {
                 respMenuRef.classList.add('lightBorder');
             }, 500);
@@ -27,6 +43,26 @@ function toogleRespMenu() {
             respMenuRef.classList.remove('slideIn');
             respMenuRef.classList.remove('lightBorder');
             contentRef.classList.remove('hideContent');
+            headerRef.classList.remove('blackBG');
         }
     }
 }
+
+function scrollHeader() {
+    const headerRef = document.getElementById('headerContainer');
+    const navRef = document.getElementById('nav');
+    const scrollHeight = window.scrollY;
+
+    if (scrollHeight > 100) {
+        console.log('start');
+        headerRef.classList.add('noShadow');
+        navRef.classList.add('navBG');
+
+    } else {
+        headerRef.classList.remove('noShadow');
+        navRef.classList.remove('navBG');
+
+    }
+
+}
+window.addEventListener('resize', resetRespMenu);
