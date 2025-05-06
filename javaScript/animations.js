9fa3ec1c20b217cb88441ece168703a2d2663e22
Johannes Roth <jrothx@icloud.com>
2025-05-06 17:48:12 +0200

    works on animations

--- javaScript/animations.js
+++ javaScript/animations.js
@@ -4,12 +4,12 @@ let hide = false;
 
 function resetRespMenu() {
     if (window.innerWidth > 780) {
-        if (hide===true) {
-         toogleRespMenu();
+        if (hide === true) {
+            toogleRespMenu();
 
         }
-       
-        
+
+
     }
 
 }
@@ -46,8 +46,8 @@ function toogleRespMenu() {
             setTimeout(() => {
                 headerRef.classList.remove('blackBG');
             }, 500);
-            
-            
+
+
         }
     }
 }
@@ -60,7 +60,7 @@ function scrollHeader() {
     const scrollHeight = window.scrollY;
 
     if (scrollHeight > 1) {
-        console.log('start');
+
         headerRef.classList.add('noShadow');
         navRef.classList.add('navBG');
 
@@ -71,4 +71,69 @@ function scrollHeader() {
     }
 
 }
+
+function dividerAnimation() {
+    const dividers = document.getElementsByClassName('divider');
+    for (let index = 0; index < dividers.length; index++) {
+        const divider = dividers[index];
+        const dividerRect = divider.getBoundingClientRect().top;
+        console.log(dividerRect);
+        const trigger = window.innerHeight * 0.5;
+        const scrollbarRect = document.getElementById('scrollBarInner').getBoundingClientRect().bottom;
+        if (dividerRect < scrollbarRect) {
+            divider.classList.add('width');
+        } else {
+            divider.classList.remove('width');
+        }
+    }
+}
+
+
+function sliderAnimations() {
+    letteringAnimation('aboutme', 'aboutmeLettering', 'aboutmeText');
+    letteringAnimation('skills', 'skillsLettering', 'skillContent');
+}
+
+function letteringAnimation(divID, sliderID, contentID) {
+    const section = document.getElementById(divID);
+    const sectionRect = section.getBoundingClientRect().top;
+    const lettering = document.getElementById(sliderID);
+    const trigger = window.innerHeight * 0.5;
+    if (sectionRect < trigger) {
+        lettering.classList.add('letteringSlide');
+        setTimeout(() => {
+            lettering.classList.add('sliderOpacity');
+        }, 1000);
+    } else {
+        lettering.classList.remove('letteringSlide');
+        lettering.classList.remove('sliderOpacity')
+
+    }
+    opacityAnimation(sectionRect, trigger,contentID);
+}
+
+
+
+
+
+
+function opacityAnimation(sectionRect, trigger, contentID) {
+   
+    const text = document.getElementById(contentID);
+
+  
+    if (sectionRect < trigger) {
+        text.classList.add('textOpacity');
+    } else {
+        text.classList.remove('textOpacity');
+    }
+
+}
+
 window.addEventListener('resize', resetRespMenu);
+window.addEventListener('scroll', () => {
+    // opacityAnimation();
+
+    // dividerAnimation();
+    sliderAnimations();
+});
